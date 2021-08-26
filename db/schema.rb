7b75e345b046107ef1c02a468cd2c42a5ab269ad
Emmanuel Sanchez <fede@fede.website>
2021-08-26 00:29:03 +0000

    testing rollback

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_235905) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
