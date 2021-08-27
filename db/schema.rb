58421776635909cff1fcbb30f96e3d48c40b8c94
Emmanuel Sanchez <fede@fede.website>
2021-08-27 01:38:35 +0000

    it is working :) after remove to columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_235905) do
+ActiveRecord::Schema.define(version: 2021_08_26_002931) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
