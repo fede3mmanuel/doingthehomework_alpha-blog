1b2ee99be8d96ed58f095c76c1d952eba8108c88
Emmanuel Sanchez <fede@fede.website>
2021-09-01 18:56:00 +0000

    refactor the article find on articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,10 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
+    
     def show
-        @article = Article.find(params[:id])
+        
     end
     
     def index
@@ -13,7 +16,7 @@ class ArticlesController < ApplicationController
     end
     
     def edit
-        @article = Article.find(params[:id])
+        
     end
     
     def create
@@ -27,7 +30,7 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        @article = Article.find(params[:id])
+       
         
         if @article.update(params.require(:article).permit(:title, :description))
             flash[:notice] = "Article was updated successfully."
@@ -38,8 +41,14 @@ class ArticlesController < ApplicationController
     end
     
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         redirect_to articles_path
+    end
+    
+    private
+    
+    def set_article
+        @article = Article.find(params[:id])
     end
 end
