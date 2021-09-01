7090adea0071c0b26397645f063cf973bc6913ed
Emmanuel Sanchez <fede@fede.website>
2021-09-01 19:01:43 +0000

    article require refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
     end
     
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = "Article was created successfully."
             redirect_to @article
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
     def update
        
         
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
@@ -50,5 +50,9 @@ class ArticlesController < ApplicationController
     
     def set_article
         @article = Article.find(params[:id])
+    end
+    
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
